e3fc2b35f78e09416ca5a837c279570f5f4e3ba3
Ekaterina Kalinina <ekaterina.kalinina.1998@gmail.com>
2018-10-25 22:32:19 +0300

    Merged master fixed conflict

--- NET.W.2018.Kalinina.01/MultyFileAssembly/fclasscar.cs
+++ NET.W.2018.Kalinina.01/MultyFileAssembly/fclasscar.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System;
 
 namespace Auto
@@ -10,17 +9,3 @@ namespace Auto
 			Console.WriteLine("Audi A8");
 		}
 	}
-=======
-using System;
-
-namespace Auto
-{
-	public class FClassCar
-	{
-		public void FClassCarInfo()
-		{
-			Console.WriteLine("Audi A8");
-		}
-	}
->>>>>>> 6b86101af58af5b06ab4f0f8d4e1ea0410005e4d
-}
