c0bf9a1b2e1a86612adb614fad794153ae0b9267
Ekaterina Kalinina <ekaterina.kalinina.1998@gmail.com>
2018-10-25 22:33:32 +0300

    Merged master fixed conflict

--- NET.W.2018.Kalinina.01/MultyFileAssembly/roadster.cs
+++ NET.W.2018.Kalinina.01/MultyFileAssembly/roadster.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System;
 
 namespace Auto
@@ -10,17 +9,4 @@ namespace Auto
 			Console.WriteLine("BMV Z4");
 		}
 	}
-=======
-using System;
-
-namespace Auto
-{
-	public class Roadster
-	{
-		public void RoadsterInfo()
-		{
-			Console.WriteLine("BMV Z4");
-		}
-	}
->>>>>>> 6b86101af58af5b06ab4f0f8d4e1ea0410005e4d
 }
